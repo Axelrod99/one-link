ad69d568b6fad63d7f953e1204a507fb82106550
jibola <ajibolaajiboye99@gmail.com>
2022-12-20 14:45:22 +0100

    pages and layout created

---
+++ my-app/src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../component/layout'
+import HomePage from './home'
+
+
+const Home = () => {
+    return (
+
+        <Layout>
+            <HomePage/>
+        </Layout>
+
+    )
+}
+
+export default Home
